a81200c2ab0f4742d83503a355ca73bf4972eab1
mikechenczy <88608181+mikechenczy@users.noreply.github.com>
2024-05-12 09:03:14 +0800

    Initial commit

---
+++ src/main/java/com/shin/graalvm/DemoApplication.java
@@ -0,0 +1,15 @@
+package com.shin.graalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import xyz.erupt.core.annotation.EruptScan;
+
+@SpringBootApplication
+@EntityScan
+@EruptScan
+public class DemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
